f2e60b55fca4f8f22cee713c8c2d2fe41d96a35b
GeirOwe <geir.owe@hotmail.com>
2021-01-20 14:51:16 +0100

    updated the auth function
    updated the auth function

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 #auth done thru azure AD 
 
 import os
-#the library in python 3.x best suited to read REST API
-import requests
+import requests                         #the library in python 3.x best suited to read REST API
 
 #start function
 def check_error_msg(response):
@@ -12,7 +11,7 @@ def check_error_msg(response):
         err_msg = 'for some reason we can not provide the data today - even if the response is: ' + response.reason
     #an error occurred
     elif response.status_code == 403:
-        err_msg = 'Most likely some roles are missing in SAP backend ERP. Error code ' + str(response.status_code) + ' - ' + response.reason
+        err_msg = 'The Auth token is ok. Most likely some roles are missing in SAP backend ERP. Error code ' + str(response.status_code) + ' - ' + response.reason
     elif response.status_code == 401:
         err_msg = 'Most likely the token has gotten a timeout, refresh the token. Error code ' + str(response.status_code) + ' - ' + response.reason
     else :
@@ -21,7 +20,16 @@ def check_error_msg(response):
 #end function
 
 #start function
+def get_token():
+    theToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiI0ODkyNmFiYi0yYzU4LTRkNmMtYWJmNy1kNjk1ZDAxYmM2YzUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8zYWE0YTIzNS1iNmUyLTQ4ZDUtOTE5NS03ZmNmMDViNDU5YjAvIiwiaWF0IjoxNjExMTQ5NzA2LCJuYmYiOjE2MTExNDk3MDYsImV4cCI6MTYxMTE1MzYwNiwiYWNyIjoiMSIsImFpbyI6IkUySmdZREJlTSt0b1FsQkZvdjNERzI5Y3psUXQrVjA5WDY4eVpJbm00amVPRnJvWFUyVUIiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiNDg5MjZhYmItMmM1OC00ZDZjLWFiZjctZDY5NWQwMWJjNmM1IiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJXw6Zyc2xhbmQiLCJnaXZlbl9uYW1lIjoiR2VpciBPd2UiLCJpcGFkZHIiOiI5Mi4yMjEuNzUuODkiLCJuYW1lIjoiR2VpciBPd2UgV8OmcnNsYW5kIiwib2lkIjoiMmJiYzgyOWMtMjY1OS00ODhhLWJlOTAtMmFmODgyMjQ3NDBjIiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTIyMDUyMzM4OC0xMDg1MDMxMjE0LTcyNTM0NTU0My0xMzc4NSIsInJoIjoiMC5BQUFBTmFLa091SzIxVWlSbFhfUEJiUlpzTHRxa2toWUxHeE5xX2ZXbGRBYnhzVUNBUFkuIiwic2NwIjoiVXNlci5SZWFkIiwic3ViIjoienZCci03RlQzVk51aHpoYjNUelYzRHRBbmdfaXNGVXd1T19kcF9MUGRqbyIsInRpZCI6IjNhYTRhMjM1LWI2ZTItNDhkNS05MTk1LTdmY2YwNWI0NTliMCIsInVuaXF1ZV9uYW1lIjoiR09XQGVxdWlub3IuY29tIiwidXBuIjoiR09XQGVxdWlub3IuY29tIiwidXRpIjoiVl9qZzB6ZHpza2VzOVd6OTF1VE9BQSIsInZlciI6IjEuMCJ9.iSE5iDreJSk2StTn4SA3K_FI8x_eu5ILXMga09u_jbCTqwVv_pTJZQXH_p7u5qvRbDJ96kAzK4I4htujZ_WM1JuJnnOrU2Ti7QbGcxTB4VcD45bzG3bT85YNJhEUNNUDBHInRsqx-8x-3xfsEpUTNf1TOh1MMt19FFcA0qTv9-_5v7GVa35KHs-msxw2lXULVH71OAkMZtn8ov446Gyn6iOL30QM-0A94mPjw7AyV7FBU7_rJC1Eg0bgWaJKF7KMLGnnbJkPg6KJtBa3Z9GuxH-DlXx-vpBd4WfOSmqX88KP-xcGJpyxZhMfBMbP9x15gNJmboaaZd43OQsOFW9ccQ'
+    return theToken
+#end function
+
+#start function
 def connect_to_api():
+    # get access token
+    theToken = get_token()
+    
     #URL and identity data
     #url = "https://api-dev.gateway.equinor.com/sap-api-basic/ProductSet('HT-1000')"
     url = "https://api-dev.gateway.equinor.com/sap-api-basic/ProductSet"
@@ -29,7 +37,8 @@ def connect_to_api():
         'format': 'json'
     }
     headers = {
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiI0ODkyNmFiYi0yYzU4LTRkNmMtYWJmNy1kNjk1ZDAxYmM2YzUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8zYWE0YTIzNS1iNmUyLTQ4ZDUtOTE5NS03ZmNmMDViNDU5YjAvIiwiaWF0IjoxNjExMTQzOTQ1LCJuYmYiOjE2MTExNDM5NDUsImV4cCI6MTYxMTE0Nzg0NSwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhTQUFBQWFDUUY5cGYyQk1wUXlGR0RNV2ZSdzFaY1FBazRvOWdRcHZ3RjR2a3YvTzQ9IiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6IjQ4OTI2YWJiLTJjNTgtNGQ2Yy1hYmY3LWQ2OTVkMDFiYzZjNSIsImFwcGlkYWNyIjoiMSIsImZhbWlseV9uYW1lIjoiV8OmcnNsYW5kIiwiZ2l2ZW5fbmFtZSI6IkdlaXIgT3dlIiwiaXBhZGRyIjoiOTIuMjIxLjc1Ljg5IiwibmFtZSI6IkdlaXIgT3dlIFfDpnJzbGFuZCIsIm9pZCI6IjJiYmM4MjljLTI2NTktNDg4YS1iZTkwLTJhZjg4MjI0NzQwYyIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yMjA1MjMzODgtMTA4NTAzMTIxNC03MjUzNDU1NDMtMTM3ODUiLCJyaCI6IjAuQUFBQU5hS2tPdUsyMVVpUmxYX1BCYlJac0x0cWtraFlMR3hOcV9mV2xkQWJ4c1VDQVBZLiIsInNjcCI6IlVzZXIuUmVhZCIsInN1YiI6Inp2QnItN0ZUM1ZOdWh6aGIzVHpWM0R0QW5nX2lzRlV3dU9fZHBfTFBkam8iLCJ0aWQiOiIzYWE0YTIzNS1iNmUyLTQ4ZDUtOTE5NS03ZmNmMDViNDU5YjAiLCJ1bmlxdWVfbmFtZSI6IkdPV0BlcXVpbm9yLmNvbSIsInVwbiI6IkdPV0BlcXVpbm9yLmNvbSIsInV0aSI6IlZLSXFSTnZxRlVpYXpZNmNfU214QUEiLCJ2ZXIiOiIxLjAifQ.kUcn7XmmrgqQ251LkZBL8Vk_QFqIgS0P2vp80CcvdEdNs3HRTSU-o2N1pI3EXBzluT_ioPJ-7cdGb4nA1Fndh7G2Rgfwjj_-LE1sYcumoicJQTdXNjdt6iWihxHg1z641NKPKd6W4xVYN9_-5EpAWcFXD0zNPew4dtrtP-TWDRf0207DqQ6uwJUcEH5pfV9jtaV3ufKUXr0q4vbtHSyeKB4HL8Wq60rJDmPq1u58uVAInZV6GoCPEU4KgCCEDE1VD4DKkXXvjW5uvnl8S7k3_4pmrjK6yOHaGm9K9_Q1ct98YqssfUf6wmXR69MaEn4dJ3Qt6aOjHfxz_XsewqzOIQ',
+        'Ocp-Apim-Trace': 'true',
+        'Authorization': 'Bearer ' + theToken,
         'Cookie': 'sap-usercontext=sap-client=235'
     }
     # send the request to the api
@@ -37,11 +46,11 @@ def connect_to_api():
     return response
 #end function
 
-#the main programe
-def main_program():
+#the main module
+def main_module():
     #call api
     response = connect_to_api()
-    #check response
+    #check response; if error or empty dataset received
     if (response.status_code > 299) or (len(response.json()) == 0):
         err_msg = check_error_msg(response)
         print(err_msg)
@@ -53,12 +62,12 @@ def main_program():
         print('.. print the first row of data received fom the SAP ODATA service ..\n')
         for key, values in xDict.items():
             print(key, ': ', values) 
-#end main programe
+#end main module
 
 # -- start of programme ----
 if __name__ == '__main__':
     os.system('clear')
     print(' ... ut å se etter SAP data ... ')
     print(' ------------------------------\n')
-    main_program()
+    main_module()
     print()
